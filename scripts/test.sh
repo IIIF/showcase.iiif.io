cd703e1dcb9f56f8e49a9574a7dc452241f20da4
Glen Robson <glen.robson@gmail.com>
2018-09-07 11:31:01 +0100

    Ignoring uv error as it is aviliable and working.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,6 @@
 # Run the tests
 #
 bundle exec jekyll build
-bundle exec htmlproofer --check-html ./_site
+bundle exec htmlproofer --check-html ./_site --url-ignore '//universalviewer.io/vendor/uv/lib/embed.js'
 
-./scripts/check_mixedcontent.sh 
+./scripts/check_mixedcontent.sh
