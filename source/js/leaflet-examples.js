dbb314fe535497c651075f392c47ca1fb1e7186b
Glen Robson <glen.robson@gmail.com>
2018-09-07 12:15:42 +0100

    Trying to fix ssl errors.

--- source/js/leaflet-examples.js
+++ source/js/leaflet-examples.js
@@ -10,6 +10,10 @@ stanfordMlk = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/hg676jb49
   attribution: '<a href="http://searchworks.stanford.edu/view/hg676jb4964">Martin Luther King Jr. & Joan Baez march to integrate schools, Grenada, MS, 1966</a>'
 }).addTo(map);
 
+princetonMap = L.tileLayer.iiif('https://libimages.princeton.edu/loris2/pudl0076%2Fmap_pownall%2F00000001.jp2/info.json', {
+  attribution: '<a href="http://arks.princeton.edu/ark:/88435/02870w62c">The provinces of New York and New Jersey, with part of Pensilvania, and the Province of Quebec : drawn by Major Holland, Surveyor General, of the Northern District in America. Corrected and improved, from the original materials, by Governr. Pownall, Member of Parliament, 1776</a>'
+});
+
 apostle = L.tileLayer.iiif('https://ids.lib.harvard.edu/ids/iiif/25286610/info.json', {
   attribution: '<a href="http://via.lib.harvard.edu/via/deliver/deepcontentItem?recordId=olvwork576793%2CVIT.BB%3A4906794">Apostle: Anonymous sculptor of Florence, 15th century (1401-1500)</a>'
 });
